381984f5db1148cb9aa3f165e614252e50e90daf
Daniela <daniela@gmail.com>
2014-01-20 23:45:43 +0100

    Серверот ги сервира фајловите од frontend наместо само од frontend/html

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var fs = require('fs');
 var express = require('express');
 
 var app = express();
+app.use(express.static(__dirname + "/frontend"));
 
 app.get('/', function(req, res) {
+
     res.writeHead(200, {'content-type': 'text/html'});
     res.end(fs.readFileSync(__dirname + '/frontend/html/index.html'));
 
