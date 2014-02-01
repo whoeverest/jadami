c61e9b22eb4cdc66b542774c2c39e7c6e3908f90
Daniela <daniela@gmail.com>
2014-02-01 19:27:39 +0100

    Made /recipes return decent looking recipes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var express = require('express');
 
 var helpers = require('./helpers');
+var recipes = require('./recipes');
 
 var app = express();
 app.use(helpers.logger);
@@ -16,6 +17,15 @@ app.get('/counter', function(req, res) {
     res.end(counter.toString());
 })
 
+app.get('/recipes', function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+    for (var i=0; i<recipes.length; i++) {
+        var temp = helpers.recipe_html(recipes[i]);
+        res.write(temp);
+    }
+    res.end();
+})
+
 app.post('/double_number', function(req, res) {
     var form_data = req.body;
     var result = form_data.number * 2;
@@ -28,6 +38,7 @@ app.post('/send_ingredients', function(req, res) {
     var ingredients_string = req.body.ingredients;
     var ingredients = helpers.split_and_trim(ingredients_string);
     console.log(ingredients);
+    console.log(recipes[0]);
     res.end('done');
 });
 
