e04cc840f1a6dba09b1b7494cd41d0f72bdc73ed
Daniela <daniela@gmail.com>
2014-02-01 19:24:53 +0100

    Added a helper that turns recipes into html

--- helpers.js
+++ helpers.js
@@ -10,5 +10,22 @@ module.exports = {
             array[i] = array[i].trim();
         }
         return array;
+    },
+    recipe_html: function(recipe) {
+        var html = "";
+
+        html += "<h1>" + recipe.name + "</h1>";
+        html += "<p>" + recipe.description + "</p>";
+        html += "<ul>";
+
+        for (var i = 0; i < recipe.ingredients.length; i++) {
+            html += "<li>" + recipe.ingredients[i].quantity + " " +
+                recipe.ingredients[i].quantity_type + " " +
+                recipe.ingredients[i].name + "</li>";
+        };
+
+        html += "</ul>";
+
+        return html;
     }
 }
